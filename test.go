2ed50d2753794275a71d22f5c1a73c8e5d369b11
hiyoyolumi <862127279@qq.com>
2021-11-17 22:04:45 +0800

    2021-11-17

--- test.go
+++ test.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import "fmt"
 
+type Vertex struct {
+	a, b int
+}
+
+func (m *Vertex) Request() *Vertex {
+	if m != nil {
+		m.a = 10
+		m.b = 20
+		return nil
+		// return &Vertex{
+		// 	a: 10,
+		// 	b: 20,
+		// }
+	}
+	return nil
+}
+
+func main() {
+	res := new(Vertex)
+	res.a = 1
+	res.b = 2
+	fmt.Println(res)
+	// res = res.Request()
+	res.Request()
+	fmt.Println(res)
 }
